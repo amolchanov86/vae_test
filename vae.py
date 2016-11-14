d02906c4d811edc25ef4fb041797c478b9a81be6
artem <a.molchanov86@gmail.com>
2016-11-14 09:19:09 -0800

    almost ready

--- vae.py
+++ vae.py
@@ -1,3 +1,5 @@
+
+
 from nnet_base import *
 import time
 import numpy as np
@@ -7,15 +9,18 @@ class VAE(NNbase):
     """
         NN class for VAE
     """
+    ## Activation function wrapper
     def activation(self, input, name):
         return tf.nn.elu(input, name=name)
 
+    ## Preprocessing of features
     def preproc(self, input, mean_values=[0]):
         ## Preprocessing
         with tf.variable_scope('preproc'):
-            return tf.nn.bias_add(self.x, [-1 * x for x in mean_values], name='mean_sub')
+            return tf.nn.bias_add(input, [-1 * x for x in mean_values], name='mean_sub')
 
-    def encoder_net(self, input_shape, n_z, layers, mean_values=[0], batch_norm=False, debug=False):
+    ## Encoder net
+    def encoder_net(self, n_z, layers, batch_norm=False, debug=True):
         # Current previous layer
         prev_lyr = self.x_postproc
 
@@ -32,28 +37,34 @@ class VAE(NNbase):
             prev_lyr = h_fc_drop
 
         # --- Creating latent space
-        # Mean parameters
+        # Mean of latent variables
         self.z_mean = self.fc_lyr(prev_lyr,
                                 out_num=n_z,
-                                name=("z_mean" % lyr_i),
+                                name="z_mean",
                                 keep_prob=self.keep_prob,
                                 batch_norm=batch_norm,
                                 activation_on=False,
                                 phase_train=self.phase_train)
 
-        # Mean parameters
-        self.z_sigma = self.fc_lyr(prev_lyr,
-                                out_num=n_z,
-                                name=("z_sigma" % lyr_i),
-                                keep_prob=self.keep_prob,
-                                batch_norm=batch_norm,
-                                activation_on=False,
-                                phase_train=self.phase_train)
+        # log(sigma^2) of latent variables (here I use relu to get rid of negative part, since it can not be negative)
+        self.z_log_sigma2 = tf.relu(self.fc_lyr(prev_lyr,
+                                        out_num=n_z,
+                                        name="z_log_sigma2_logit",
+                                        keep_prob=self.keep_prob,
+                                        batch_norm=batch_norm,
+                                        activation_on=False,
+                                        phase_train=self.phase_train), name='z_log_sigma2')
+
+        return (self.z_mean, self.z_log_sigma2)
 
 
-    def decoder_net(self, input_shape, n_z, layers, mean_values=[0], batch_norm=False, debug=False):
-        # Current previous layer
-        prev_lyr = self.x_postproc
+    ## Decoder net
+    def decoder_net(self, layers, batch_norm=False, debug=True):
+        # Necessary parameters
+        x_post_shape = self.x_postproc.get_shape().as_list()
+
+        # Take converted sample
+        prev_lyr = self.z
 
         # Building layers
         # Every previous layer should be flattened before entering here
@@ -67,28 +78,20 @@ class VAE(NNbase):
 
             prev_lyr = h_fc_drop
 
-        # --- Creating latent space
-        # Mean parameters
-        self.z_mean = self.fc_lyr(prev_lyr,
-                                out_num=n_z,
-                                name=("fc%02d" % lyr_i),
+        # --- Creating output space
+        # Reconstructed input
+        self.x_rec = 255 * tf.sigmoid( self.fc_lyr(prev_lyr,
+                                out_num=x_post_shape,
+                                name="x_rec_logit",
                                 keep_prob=self.keep_prob,
                                 batch_norm=batch_norm,
                                 activation_on=False,
-                                phase_train=self.phase_train)
-
-        # Mean parameters
-        self.z_sigma = self.fc_lyr(prev_lyr,
-                                out_num=n_z,
-                                name=("fc%02d" % lyr_i),
-                                keep_prob=self.keep_prob,
-                                batch_norm=batch_norm,
-                                activation_on=False,
-                                phase_train=self.phase_train)
+                                phase_train=self.phase_train), name='x_rec')
+        return self.x_rec
 
 
     ## Preinitialization (defines inputs)
-    def create_inputs(self, input_shape, out_num, debug=False):
+    def create_inputs(self, input_shape, debug=False):
         with tf.variable_scope('in'):
             # placeholders for input and output
             self.x = tf.placeholder(tf.float32, shape=input_shape, name='feat')
@@ -97,19 +100,16 @@ class VAE(NNbase):
                                   message='%s shape: ' % 'Input',
                                   summarize=4, first_n=1)
 
-            self.label_hard = tf.placeholder(tf.int32, shape=[input_shape[0]], name='label_hard')
-
             self.phase_train = tf.placeholder(tf.bool, name='phase_train')
             self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         tf.add_to_collection('in', self.x)
-        tf.add_to_collection('in', self.label_hard)
         tf.add_to_collection('in', self.phase_train)
         tf.add_to_collection('in', self.keep_prob)
 
         print 'Shape of ', 'input', ' ', self.x.get_shape()
-        print 'Shape of ', 'labels', ' ', self.label_hard.get_shape().as_list()
 
+    ## Initializes reguralizer (if needed)
     def init_reguralizer(self, weight_decay, weight_norm='l2'):
         with tf.variable_scope('loss'):
             self.weights_norm = tf.reduce_sum(
@@ -119,33 +119,38 @@ class VAE(NNbase):
                 name='weights_norm'
             )
         tf.add_to_collection('w_losses', self.weights_norm)
+        print ('Reguralizer is not set up')
 
-    def init_loss(self):
+    ## Initialization of losses
+    def init_loss(self, n_z):
         with tf.variable_scope('loss'):
-            # Hard label loss
-            self.loss_hard = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(self.y, self.label_hard),
-                                            name='loss_hard')
-            self.loss_hard_w = self.loss_hard
+            # Encoder loss (latent loss)
+            self.loss_enc = 0.5 * tf.reduce_sum(tf.reduce_sum(tf.exp(self.z_log_sigma2), 1)
+                                        + tf.reduce_sum(tf.square(self.z_mean),1)
+                                        - n_z
+                                        - tf.reduce_sum(self.z_log_sigma2, 1),  1, name='loss_enc')
 
-        # tf.scalar_summary("loss/loss_soft", self.loss_soft)
-        tf.scalar_summary("loss/loss_hard", self.loss_hard)
-        tf.add_to_collection('losses', self.loss_hard)
-        tf.add_to_collection('w_losses', self.loss_hard_w)
+            self.loss_dec = 0.5 * tf.reduce_mean(tf.square(tf.sub(self.x_postproc, self.x_rec, name='error')), name='loss_rec') / self.x_rec_sigma2
 
+        tf.scalar_summary("loss/loss_enc", self.loss_enc)
+        tf.scalar_summary("loss/loss_dec", self.loss_dec)
+
+        tf.add_to_collection('losses', self.loss_enc)
+        tf.add_to_collection('losses', self.loss_dec)
+
+        # Weighted losses will be just summed up together
+        # to create the final loss for optimization
+        tf.add_to_collection('w_losses', self.loss_enc)
+        tf.add_to_collection('w_losses', self.loss_dec)
+
+    ## Initialization of metrics
     def init_metrics(self):
-        with tf.variable_scope('out'):
-            # Metric
-            self.correct_prediction = tf.equal(tf.cast(tf.argmax(self.y, 1), tf.int32), self.label_hard,
-                                               name='correct_pred')
-            self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32), name='accuracy')
-
-        tf.scalar_summary("accuracy", self.accuracy)
-        tf.add_to_collection('out', self.accuracy)
+        print ('Metrics are not set up')
 
     ## Training initialization (defines losses and optimizer)
     def init_training(self, weight_decay, weight_norm='l2'):
         self.init_metrics()
-        self.init_reguralizer(weight_decay, weight_norm)
+        # self.init_reguralizer(weight_decay, weight_norm)
         self.init_loss()
 
         with tf.variable_scope('out'):
@@ -154,7 +159,7 @@ class VAE(NNbase):
 
         with tf.variable_scope('train'):
             # Hard labels only
-            self.train_step = tf.train.RMSPropOptimizer(1e-3, momentum=0).minimize(self.loss)
+            self.train_step = tf.train.RMSPropOptimizer(self.learning_rate, momentum=0).minimize(self.loss)
             # self.train_step = tf.train.AdagradOptimizer(1e-3).minimize(self.loss)
             # self.train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(self.loss)
 
@@ -164,117 +169,72 @@ class VAE(NNbase):
         tf.add_to_collection('train', self.train_step)
 
     ###################################################################################################################
+    def train_step_run(self, X, params):
+        """Train model based on mini-batch of input data.
 
-    ## Training validation (generates summary)
-    def train_step_run(self, data, label, params):
-        # print 'NNET: Train_step: Feat type = ', data.dtype, ' feat_shape=', data.shape
+        Return cost of mini-batch.
+        """
         start_time_inference = time.time()
-        self.sess.run(self.train_step, \
-                      feed_dict={ \
-                          self.x: data, \
-                          self.label_hard: label[0], \
-                          self.keep_prob: params['nnet_keep_prob'], \
-                          self.phase_train: True})
-        # self.train_step.run(feed_dict={
-        #     self.x: data,
-        #     self.label_hard: label[0],
-        #     self.keep_prob: params['nnet_keep_prob'],
-        #     self.phase_train: True})
+        opt, loss = self.sess.run((self.train_step, self.loss),
+                                  feed_dict={self.x: X,
+                                             self.keep_prob: params['keep_prob'],
+                                             self.phase_train: True})
         inf_time = time.time() - start_time_inference
-        return inf_time
+        return loss, inf_time
 
-    ## Validation function (no summary needed)
-    def train_run(self, data, label, params):
+    def transform(self, X):
+        """Transform data by mapping it into the latent space."""
+        # Note: This maps to mean of distribution, we could alternatively
+        # sample from Gaussian distribution
         start_time_inference = time.time()
-        pred, loss, summary_cur = self.sess.run( \
-            [self.pred, self.loss, self.summary_all], \
-            feed_dict={self.x: data, \
-                       self.label_hard: label[0], \
-                       self.keep_prob: 1.0, \
-                       self.phase_train: False} \
-            )
+        z_mean = self.sess.run(self.z_mean,
+                                feed_dict = {self.x: X,
+                                self.keep_prob: 1.0,
+                                self.phase_train: False})
         inf_time = time.time() - start_time_inference
-        error, pred_val = self.get_error(pred, label[1], params['dim_angular'])
+        return z_mean, inf_time
 
-        # print 'TRAIN: pred = ', np.argmax(pred, axis=1)
-        # print 'TRAIN: label = ', label[0]
+    def generate(self, z_mu=None ):
+        """ Generate data by sampling from latent space.
 
-        return error, pred_val, pred, loss, inf_time, summary_cur
-
-    ## Validation function
-    def val_run(self, data, label, params):
+        If z_mu is not None, data for this point in latent space is
+        generated. Otherwise, z_mu is drawn from prior in latent
+        space.
+        """
         start_time_inference = time.time()
-        pred, loss, accuracy = self.sess.run( \
-            [self.pred, self.loss, self.accuracy], \
-            feed_dict={self.x: data, \
-                       self.label_hard: label[0], \
-                       self.keep_prob: 1.0, \
-                       self.phase_train: False} \
-            )
+        if z_mu is None:
+            z_mu = np.random.normal(size=self.architecture["n_z"])
+        # Note: This maps to mean of distribution, we could alternatively
+        # sample from Gaussian distribution
+        x_rec = self.sess.run(self.x_rec,
+                                feed_dict={self.z: z_mu,
+                                self.keep_prob: 1.0,
+                                self.phase_train: False})
         inf_time = time.time() - start_time_inference
-        error, pred_val = self.get_error(pred, label[1], params['dim_angular'])
+        return x_rec, inf_time
 
-        # print 'pred = ', pred
-        # print 'label = ', label[0]
-        # print 'gt = ', label[1]
-        # print 'VAL: pred = ', np.argmax(pred, axis=1)
-        # for i in range(0, y.shape[0]):
-        #     print y[i], ' ',
-        # print
-        # print 'VAL: label = ', label[0]
-
-        return error, pred_val, pred, loss, inf_time, accuracy
-
-    ## Just prediction function
-    def pred_run(self, data, params):
+    def reconstruct(self, X):
+        """ Use VAE to reconstruct given data. """
         start_time_inference = time.time()
-        pred = self.sess.run( \
-            [self.pred], \
-            feed_dict={self.x: data, \
-                       self.keep_prob: 1.0, \
-                       self.phase_train: False}
-        )
+        x_rec = self.sess.run(self.x_rec,
+                                feed_dict = {self.x: X,
+                                self.keep_prob: 1.0,
+                                self.phase_train: False})
         inf_time = time.time() - start_time_inference
-        return pred, inf_time
 
-    def setBinValues(self, bin_values):
-        self.bin_values = bin_values
-
-    ## Pred to values conversion
-    def pred2val(self, pred):
-        # print 'Pred = ', pred, ' shape =', pred.shape, ' type=', type(pred)
-        pred_label = np.argmax(pred, axis=1)
-        return self.bin_values[pred_label]
-
-    def pred2lbl(self, pred):
-        return np.argmax(pred, axis=1)
-
-    ## Test calculation
-    def get_error(self, pred, gt, angular):
-        pred_val = self.pred2val(pred)
-        error = gt - pred_val
-        # if we have angular coordinates we do correction, because abs error cannot be > 180 deg
-        if angular:
-            err_wrong_mask = np.abs(error) > 180.0
-            err_wrong = error[err_wrong_mask]
-            error[err_wrong_mask] = -np.sign(err_wrong) * (360.0 - np.abs(err_wrong))
-        return error, pred_val
-
-    ## Testing
-    def test_run(self, data, label, params):
-        pred, inf_time = self.pred_run(data, params)
-        error, pred_val = self.get_error(pred, label[1], params['dim_angular'])
-        return error, pred_val, pred, inf_time
+        return x_rec, inf_time
 
     def init_session(self):
         self.sess = tf.InteractiveSession()
 
     ## Class constructor
     # @param input_shape  [h,w] of the input features
-    # @param out_num  Number of output classes
-    # @param layers   list containing number of neurons per layer
-    def __init__(self, input_shape=None, out_num=None, layers=None, n_z=20, batch_norm=False, debug=False, weight_decay=0.0005,
-                 weight_norm='l2', init=True):
+    # @param architecture   dictionary ('encoder','decoder', 'n_z') containing lists of number of neurons in every layer
+    def __init__(self, input_shape=None, architecture=None, learning_rate=1e-3,
+                 batch_norm=False, debug=False,
+                 weight_decay=0.0005, weight_norm='l2',
+                 init=True):
+
         # Calling constructor of the base class
         NNbase.__init__(self)
 
@@ -283,23 +243,45 @@ class VAE(NNbase):
 
         # Init
         if init:
+            # Parameters
+            self.x_rec_sigma2 = 1 # Reconstruction sigma
+            self.learning_rate = learning_rate # Initial learning rate
+            self.architecture = architecture
+            self.batch_size = self.x.get_shape().as_list()[0]
 
+            # --- Architecture initialization
             # Creating inputs and groundtruth placeholders
-            self.create_inputs(input_shape, out_num, debug=debug)
+            self.create_inputs(input_shape, debug=debug)
 
             # Preprocessing
             self.x_postproc = self.preproc(self.x, mean_values=[0])
 
-            self.build_model(input_shape=[None, input_shape[0], input_shape[1], 1],
-                             out_num=out_num,
-                             layers=layers,
-                             batch_norm=batch_norm,
-                             debug=debug)
+            # Initilization of net architecture
+            self.encoder_net(self, n_z=architecture['n_z'],
+                             layers=architecture['encoder'],
+                             batch_norm=batch_norm, debug=debug)
 
+            # Drawing samples for decoder (just 1 sample for now)
+            self.eps = tf.random_normal(shape=(self.batch_size, architecture['n_z']),
+                                   mean=0, stddev=1,
+                                   dtype=tf.float32, name='eps')
+
+            # Transforming eps to z (according to re-parametrization trick)
+            # z = mu + sigma*epsilon
+            self.z = tf.add(self.z_mean,
+                            tf.mul(tf.sqrt(tf.exp(self.z_log_sigma2)), self.eps, name='sigmaXeps'), name='z')
+
+            # Decoder (generator or reconstruction) network
+            self.decoder_net(layers=architecture['decoder'],
+                             batch_norm=batch_norm, debug=debug)
+
+            # Initialization of training (losses, optimizer etc.)
             self.init_training(weight_decay=weight_decay, weight_norm=weight_norm)
 
-            # Some statistics:
-            print 'NNET: Parameters num = ', self.params_num
+
+            # --- Other initialization and printing
+            # Reporting statistics:
+            print 'NNET: Parameters num = ', self.params_num # Number of parameters in the network
 
             # start session
             print 'NNET: Initializing default session ...'
@@ -307,3 +289,7 @@ class VAE(NNbase):
 
             # All summaries
             self.summary_all = tf.merge_all_summaries()
+
+            # Initializing all tensors (variables)
+            print 'NNET: Initializing all variables ...'
+            tf.initialize_all_variables()
--- vae_train.py
+++ vae_train.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import argparse
+
 from vae import *
 
 # Loading MNIST
@@ -9,3 +12,73 @@ n_samples = mnist.train.num_examples
 np.random.seed(0)
 tf.set_random_seed(0)
 
+
+def train(network_architecture, learning_rate=0.001,
+          batch_size=100, training_epochs=10, display_step=5):
+
+    vae = VAE(input_shape=(None, 28, 28),
+             architecture=network_architecture,
+             learning_rate=learning_rate,
+             batch_norm=False,
+             debug=True)
+
+    params = {}
+    params['keep_prob'] = 0.5
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(n_samples / batch_size)
+
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, _ = mnist.train.next_batch(batch_size)
+
+            # Fit training using batch data
+            cost, time = vae.train_step_run(batch_xs, params=params)
+
+            # Compute average loss
+            avg_cost += cost / n_samples * batch_size
+
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch+1), \
+                  "cost=", "{:.9f}".format(avg_cost)
+    return vae
+
+def test_reconstruct(vae):
+    x_sample = mnist.test.next_batch(100)[0]
+    x_reconstruct = vae.reconstruct(x_sample)
+
+    plt.figure(figsize=(8, 12))
+    for i in range(5):
+        plt.subplot(5, 2, 2 * i + 1)
+        plt.imshow(x_sample[i].reshape(28, 28), vmin=0, vmax=1)
+        plt.title("Test input")
+        plt.colorbar()
+        plt.subplot(5, 2, 2 * i + 2)
+        plt.imshow(x_reconstruct[i].reshape(28, 28), vmin=0, vmax=1)
+        plt.title("Reconstruction")
+        plt.colorbar()
+    plt.tight_layout()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    # Parameters
+    architecture = {}
+    architecture['encoder'] = [500, 500, 250]
+    architecture['decoder'] = [250, 500, 500]
+    architecture['n_z'] = 20
+
+    # Start training
+    vae = train(network_architecture=architecture)
+
+    # Testing
+    test_reconstruct(vae)
+
+
+if __name__ == "__main__":
+    main()
